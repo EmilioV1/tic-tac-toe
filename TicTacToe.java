5a6a23611d532c9cd40514d43498f625982096f3
Emilio Valladares <vallae.wumbo@gmail.com>
2020-01-19 17:02:22 -0800

    Initializing repo and created board for game.

---
+++ TicTacToe.java
@@ -0,0 +1,19 @@
+
+/**
+ * The goal is to get 3 in a row.
+ * 
+ * @author Emilio Valladares 
+ * @version 1/19/2020
+ */
+public class TicTacToe {
+    
+    public static void main(String[] args) {
+        
+        char[][] gameBoard = 
+        {{' ', '|', ' ', '|', ' '}, 
+         {'-', '+', '-', '+', '-'}, 
+         {' ', '|', ' ', '|', ' '}, 
+         {'-', '+', '-', '+', '-'}, 
+         {' ', '|', ' ', '|', ' '}};
+    }
+}
