3e779c6c7a8d72d76417759d1c96a9920036334f
Emilio Valladares <vallae.wumbo@gmail.com>
2020-01-20 00:20:55 -0800

    Added switch cases for rendering X in each position. Program ends after each individual input.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * The goal is to get 3 in a row.
  * 
@@ -6,7 +6,6 @@
  * @version 1/19/2020
  */
 public class TicTacToe {
-    
     public static void main(String[] args) {
         char[][] gameBoard = 
         {{' ', '|', ' ', '|', ' '}, 
@@ -15,6 +14,44 @@ public class TicTacToe {
          {'-', '+', '-', '+', '-'}, 
          {' ', '|', ' ', '|', ' '}};
          
+        printGameBoard(gameBoard);
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9):");
+        int pos = scan.nextInt();
+        
+        System.out.println(pos);
+        
+        switch(pos) {
+            case 1:
+                gameBoard[0][0] = 'X';
+                break;
+            case 2:
+                gameBoard[0][2] = 'X';
+                break;
+            case 3:
+                gameBoard[0][4] = 'X';
+                break;
+            case 4:
+                gameBoard[2][0] = 'X';
+                break;
+            case 5:
+                gameBoard[2][2] = 'X';
+                break;
+            case 6:
+                gameBoard[2][4] = 'X';
+                break;
+            case 7:
+                gameBoard[4][0] = 'X';
+                break;
+            case 8:
+                gameBoard[4][2] = 'X';
+                break;
+            case 9:
+                gameBoard[4][4] = 'X';
+                break;
+        }
+        
         printGameBoard(gameBoard);
     }
     
