6fb278b4c9c98ca67594e55826e4c4eeeb4386c6
Emilio Valladares <vallae.wumbo@gmail.com>
2020-01-29 17:00:45 -0800

    Able to track positioning of placed pieces and print as well as random cpu selections.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 /**
  * The goal is to get 3 in a row.
  * 
@@ -16,43 +17,19 @@ public class TicTacToe {
          
         printGameBoard(gameBoard);
         
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your placement (1-9):");
-        int pos = scan.nextInt();
-        
-        System.out.println(pos);
-        
-        switch(pos) {
-            case 1:
-                gameBoard[0][0] = 'X';
-                break;
-            case 2:
-                gameBoard[0][2] = 'X';
-                break;
-            case 3:
-                gameBoard[0][4] = 'X';
-                break;
-            case 4:
-                gameBoard[2][0] = 'X';
-                break;
-            case 5:
-                gameBoard[2][2] = 'X';
-                break;
-            case 6:
-                gameBoard[2][4] = 'X';
-                break;
-            case 7:
-                gameBoard[4][0] = 'X';
-                break;
-            case 8:
-                gameBoard[4][2] = 'X';
-                break;
-            case 9:
-                gameBoard[4][4] = 'X';
-                break;
-        }
-        
-        printGameBoard(gameBoard);
+        while(true) {
+            Scanner scan  = new Scanner(System.in);
+            System.out.println("Enter your placement (1-9):");
+            int playerPos = scan.nextInt();
+            
+            placePiece(gameBoard, playerPos, "player");
+            
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9) +1;
+            placePiece(gameBoard, cpuPos, "cpu");
+    
+            printGameBoard(gameBoard);
+        } 
     }
     
     public static void printGameBoard(char[][] gameBoard) {
@@ -66,7 +43,7 @@ public class TicTacToe {
     
     public static void placePiece(char[][] gameBoard, int pos, String user) {
         
-        char symbol = 'X';
+        char symbol = ' ';
         
         if(user.equals("player")) {
             symbol = 'X';
